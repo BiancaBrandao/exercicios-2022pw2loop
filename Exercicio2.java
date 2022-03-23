4d8ce3043ebc296a5e066558c7d3507227bd87e8
BiancaBrandao <biancabrandao199@gmail.com>
2022-03-23 17:57:12 +0000

    criação de vetor

---
+++ Exercicio2.java
@@ -0,0 +1,18 @@
+public class Exercicio2 {
+    public static void main(String[] args) {
+        int vetor[] = {15, 20, 55, 10, 85, 36, 77, 56, 66, 25};
+        int menorValor = vetor[0];
+
+        for(int i = 0;i < 10;i++) {
+            if(vetor[1] < menorValor) {
+                menorValor = vetor[1];
+            }
+            if(vetor)[i] > maiorValor)
+                maiorValor = vetor[i];
+
+            System.out.println("Menor Valor,")
+            System.out.prinyln("Maior valor")
+            System.out.println(vetor[i]);
+        }
+    }
+}
